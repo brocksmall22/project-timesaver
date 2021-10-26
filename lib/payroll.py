26f990a3a458470706764bc6b6f239dd1b3b2403
Dylan Rankin <drankin@bsu.edu>
2021-10-26 18:11:24 -0400

    made changes to get empnumber and get payrate

--- lib/payroll.py
+++ lib/payroll.py
@@ -1,12 +1,11 @@
 from openpyxl import load_workbook
 
-# having trouble with import on PC
 
 fileList = []
 returnArray = []
 
 # returns the file list as an array using " "as a seperator between file names
-
+#most likley will be deprecated in the future mainly used for testing
 
 def getfilelist(fileString):
     fileList = fileString.split(" ")
@@ -22,25 +21,31 @@ def loadWorkBooks(fileList):
         readWorkBook(wb)
 
 # reads an indiual work book then prints the resulting values from in the range of cells A21->F55
-# this still needs works with openpyxl as it  has trouble starting i do not know which values it returns yet
+#issues with 
 
 
 def readWorkBook(wb):
     
-    getEmpNum(wb)
-    getPayRate(wb)
     getRunInfo(wb)
     
-
-def getEmpNum(wb):
+    getEmpinfo(wb)
+    
+    
+    
+#this gets and returns the pay rate and employe number for those on run
+def getEmpinfo(wb):
     sheet = wb.active
-    for i1 in sheet["A21:F55"]:
+    for i1 in sheet["A21:h55"]:
         
         if i1[5].value == 1:
             
             returnSting = wb["Pay"][i1[0].value.split("!")[1]].value
             print("Emp Num: "+str(returnSting))
+            returnSting = wb["Pay"][i1[7].value.split("!")[1]].value
+            print("PayRate: "+str(returnSting))
 
+
+#this is no longer needed 
 def getPayRate(wb):
     sheet = wb.active
     for i1 in sheet["F21:H55"]:
