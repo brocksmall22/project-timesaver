ce58e4f5758f6c4b9128b477b1efbd260b4b5797
Brock Small <bwsmall@bsu.edu>
2021-12-07 15:06:02 -0500

    Added "config_manager.py", which will intake a configuration file and provide a getter method for the file path.

---
+++ lib/config_manager.py
@@ -0,0 +1,32 @@
+import json
+from os import getenv
+from openpyxl import load_workbook
+
+configFile = ""
+folder_path = ""
+
+##Detect if Configureation File is present
+
+def isPresent():
+    if(configFile == ""):
+        return False
+    else:
+        return True
+
+##Instantiate the config file and set variables
+
+def setConfig():
+    configFile = load_workbook(getenv('APPDATA') + "\\project-time-saver\\config.json")
+    print("There was an error setting the config file.")
+    data = json.load(configFile)
+    folder_path = data['folder_path']
+    
+##Checks if the config file has been created, then returns the Folder Path
+
+def get_folderPath():
+    if(isPresent() == True):
+        return folder_path
+    
+
+
+##load_workbook(getenv('APPDATA') + "\\project-time-saver\\config.json")
