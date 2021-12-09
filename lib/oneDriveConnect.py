78af2117160f2247b04b3cab93c2d84e851156c0
Dylan Rankin <drankin@bsu.edu>
2021-12-09 15:39:35 -0500

    made extensionStripper
    this function given the file path strips the path and the file extenstion and returns the run number

--- lib/oneDriveConnect.py
+++ lib/oneDriveConnect.py
@@ -14,10 +14,25 @@ class oneDriveConnect:
             if file.endswith(".xlxs"):
                 oneDriveConnect.files.apened(os.path.join(path,file))
         for i in oneDriveConnect.files:
-            print(i)
+            oneDriveConnect.getLastModifiedDate(i)
 
+
+    """
+    getLastModifiedDate(file)
+    this gets the last modified date of the file with the given path
+    """
     def getLastModifiedDate(file):
        return(os.path.getmtime(file))
 
-    def main():
-        oneDriveConnect.getFiles()
+
+    """
+    extensionStripper(file)
+    this function given the file path strips the path and the file extenstion and returns the run number
+    """
+    def extensionStripper(file):
+        os.path.splitext(file)
+        return(file.split("/")[-1])
+
+
+
+
