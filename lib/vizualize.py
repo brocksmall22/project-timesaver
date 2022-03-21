6aa84ec01e5629b8ddd9761b08fa7d7641221946
Dalton Craig <djcraig@bsu.edu>
2022-03-21 07:04:24 -0700

    Adds basic framework for the visualization class

---
+++ lib/vizualize.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+from .sqlFunctions import sqlFunctions
+
+class visualize:
+    def __init__(self, startDate, endDate):
+        self.startDate = startDate
+        self.endDate = endDate
+
+    
+    def plotTypesOfRuns():
+        pass
+
+
+    def plotRunStartTimeDistribution():
+        pass
+
+
+    def plotRunTownships():
+        pass
+
+
+    def plotRunStartTimesByTypes():
+        pass
+
+
+    def plotApparatusUsageFrequency():
+        pass
+
+
+    def plotGivenAid():
+        pass
+
+
+    def plotTakenAid():
+        pass
+
+
+    def plotAverageRunTimes():
+        pass
+
+
+    def plotShiftCoverage():
+        pass
